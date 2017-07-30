588ad57c2db2e481464176b4acc4f60a2926b72e
Orest Patlyka <orest.patlyka@gmail.com>
2017-07-30 20:03:37 +0300

    Add SwiftLesson01Basics.playground

---
+++ SwiftLesson01Basics.playground/Contents.swift
@@ -0,0 +1,32 @@
+
+
+/*let redColour = "red"
+let greenColour = "green"
+let blueColour = "blue"
+
+var colour : String
+
+colour = redColour
+print(colour)
+
+// uncomment to see a nice print
+print("Red colour value = \(redColour)\nGreen color value = \(greenColour)")
+*/
+
+/*
+>>>>>>>>>>>>>>>
+HOMEWORK
+>>>>>>>>>>>>>>>
+*/
+
+let firstName = "Орест"
+let lastName = "Патлика"
+let weight = 72
+let age = 19
+let height = 183
+
+print("Имя студента: \(firstName);")
+print("Фамилия студента: \(lastName);")
+print("Возраст студента: \(age) лет;")
+print("Рост студента: \(height) см;")
+print("Вес студента: \(weight) кг;")
